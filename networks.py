2a05e619b49db2de133c8e399be650ef34224c79
Zachary <zacharypreston1996@protonmail.com>
2021-09-21 10:56:07 -0400

    formatted print statement for the MAC address. Source code found on GeekstoGeeks.com

--- networks.py
+++ networks.py
@@ -2,6 +2,8 @@
 # in a formatted manner
 
 import uuid
-
+print ("Formatted MAC address : ")
+print (':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff)
+for elements in range(0,2*6,2)][::-1]))
 print(hex(uuid.getnode()))
 # f8:75:a4:66:97:ce
